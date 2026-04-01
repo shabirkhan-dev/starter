ac9494520779c563657c43154a64aa93d1d3b5ef
shabirkhan-dev <shabirkhan.dev@gmail.com>
2026-04-01 19:26:53 +0500

    feat: add stylua

--- scripts/lua/tests/main_test.lua
+++ scripts/lua/tests/main_test.lua
@@ -2,7 +2,7 @@ local has_lua = os.execute("command -v lua >/dev/null 2>&1")
 local lua_cmd = (has_lua == true or has_lua == 0) and "lua" or "lua5.4"
 local handle = io.popen(lua_cmd .. " scripts/lua/main.lua")
 if handle == nil then
-	error("failed to execute lua main script")
+    error("failed to execute lua main script")
 end
 
 local output = handle:read("*a")
